1770da3dd61f8c3b8c3dafd3426b9f97923f4ac7
Gyanesh <gyaneshhere@users.noreply.github.com>
2026-03-01 22:48:17 -0800

    Create constants.js
    Added ML_BUILD_TARGET and ML_DEPLOY_TARGET to supported env vars map

---
+++ generators/app/lib/constants.js
@@ -0,0 +1,7 @@
+export const SUPPORTED_ENV_VARS = {
+  ML_INSTANCE_TYPE:  'instanceType',
+  ML_BUILD_TARGET:   'buildTarget',    // NEW
+  ML_DEPLOY_TARGET:  'deployTarget',   // NEW
+  AWS_REGION:        'awsRegion',
+  AWS_ROLE:          'roleArn',
+};
